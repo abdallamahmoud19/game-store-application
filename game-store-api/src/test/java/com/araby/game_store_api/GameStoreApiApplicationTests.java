de462680141de9eedba0476985408ff11b0848e0
abdallaaraby19 <abdallamahmoud642@gmail.com>
2026-07-15 21:52:45 +0300

    initializing the backend project

---
+++ game-store-api/src/test/java/com/araby/game_store_api/GameStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araby.game_store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
