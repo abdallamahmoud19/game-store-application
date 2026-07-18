92b279bea17ee2cb07cd149e653a7a0e7bca06a3
abdallaaraby19 <abdallamahmoud642@gmail.com>
2026-07-18 20:44:48 +0300

    Adding Menu entity.

--- game-store-api/src/main/java/com/araby/game_store_api/menu/Menu.java
+++ game-store-api/src/main/java/com/araby/game_store_api/menu/Menu.java
@@ -1,23 +1,31 @@
-package com.araby.game_store_api.comment;
+package com.araby.game_store_api.menu;
 
 import com.araby.game_store_api.common.BaseEntity;
-import com.araby.game_store_api.game.Game;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
+
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Comment extends BaseEntity {
-    private String content;
+public class Menu extends BaseEntity {
+    private String label;
     @ManyToOne
-    @JoinColumn(name = "game_id")
-    private Game game;
+    @JoinColumn(name = "parent_id")
+    private Menu parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Menu> subMenus;
+
+
 }
